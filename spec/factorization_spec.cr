0fd346bad8e00e976e52877831a5369a883086e2
Nelson Costa <nelson.costa85@gmail.com>
2018-08-17 10:08:49 +0100

    Added prime factorization methods

--- spec/factorization_spec.cr
+++ spec/factorization_spec.cr
@@ -2,12 +2,23 @@ require "spec"
 require "../src/factorization.cr"
 
 describe Math do
-  it "should calculate correctly primes" do
-    Math.isPrime(-1).should eq(false)
-    Math.isPrime(0).should eq(false)
-    Math.isPrime(1).should eq(false)
-    Math.isPrime(2).should eq(true)
-    Math.isPrime(5).should eq(true)
-    Math.isPrime(6).should eq(false)
+  it "should identify primes" do
+    Math.prime?(-1).should eq(false)
+    Math.prime?(0).should eq(false)
+    Math.prime?(1).should eq(false)
+    Math.prime?(2).should eq(true)
+    Math.prime?(5).should eq(true)
+    Math.prime?(6).should eq(false)
+  end
+
+  it "should compute primes" do
+    Math.computePrimes(10).should eq([2, 3, 5, 7])
+    Math.computePrimes(20).should eq([2, 3, 5, 7, 11, 13, 17, 19])
+  end
+
+  it "should factorize numbers" do
+    Math.factorize(10).should eq([2, 5])
+    Math.factorize(33).should eq([3, 11])
+    Math.factorize(238).should eq([2, 7, 17])
   end
 end
--- src/factorization.cr
+++ src/factorization.cr
@@ -1,8 +1,22 @@
 module Math
-  def isPrime(n)
+  def prime?(n)
     return false if n < 2
     return true if n == 2
     (2..Math.sqrt(n) + 1).each { |i| return false if n % i == 0 }
     return true
+  end
+
+  def computePrimes(max)
+    (2..max).select { |n| prime?(n) }
+  end
+
+  def factorize(n)
+    factors = Array(Int32).new
+    computePrimes(n).each do |i|
+      if (n % i == 0)
+        factors << i
+      end
+    end
+    return factors
   end
 end
