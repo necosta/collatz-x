baccf0bb0bd0523a4cc298fb4ccb3b003ba94c6c
Nelson Costa <nelson.costa85@gmail.com>
2018-08-21 22:55:06 +0100

    Added args option parser

--- src/entrypoint/main.cr
+++ src/entrypoint/main.cr
@@ -1,7 +1,32 @@
+require "option_parser"
 require "../main"
 
-maxCycles = 1
-maxPermutations = 2
-(1..maxPermutations).each do |p|
-  Math.run(maxCycles, p)
+enum Option
+  Collatz  # 0
+  Analysis # 1
+end
+
+out = Option::Collatz
+
+OptionParser.parse! do |parser|
+  parser.banner = "Usage:"
+  parser.on("-o option", "--option option", "Selects a run option") { |option| out = Option.parse(option) }
+  parser.on("-h", "--help", "Shows this help") { puts parser }
+  parser.invalid_option do |flag|
+    STDERR.puts "ERROR: #{flag} is not a valid option."
+    STDERR.puts parser
+    exit(1)
+  end
+end
+
+if (out == Option::Collatz)
+  puts "Running collatz function"
+  # ToDo...
+else
+  puts "Running analysis"
+  maxCycles = 1
+  maxPermutations = 2
+  (1..maxPermutations).each do |p|
+    Math.run(maxCycles, p)
+  end
 end
